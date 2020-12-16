6c7169915bdacaa7d0e5a24ff3b11c83c53e2607
xiaoyown <qzy09101018@sina.com>
2020-12-16 19:39:34 +0800

    fix

--- src/AxisBoard/AxisBoard/index.jsx
+++ src/AxisBoard/AxisBoard/index.jsx
@@ -1,13 +1,32 @@
+import './index.less';
 import React from 'react';
 
 class PageAxis extends React.Component {
   constructor(props) {
     super(props);
   }
+  state = {
+    scale: 1
+  }
+  static getDerivedStateFromProps (props) {
+    const { width, height, scale } = props;
+    return {
+      deriveStyle: {
+        width: width * scale + 'px',
+        height: height * scale + 'px',
+        backgroundColor: 'blue'
+      }
+    };
+  }
   componentDidMount() {
   }
   render() {
-    return <div className="xvd-axis-board">
+    const { deriveStyle } = this.state;
+    return <div className="xvf-axis-board">
+      <div
+        className="xvf-axis-board-canvas"
+        style={deriveStyle}
+      ></div>
     </div>
   }
 };
--- src/AxisBoard/index.jsx
+++ src/AxisBoard/index.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
 import './index.less'
+import React from 'react'
+import AxisBoard from './AxisBoard'
 
 class PageAxisBoard extends React.Component {
   constructor(props) {
     super(props);
   }
+  state = {
+    scale: 1
+  }
   componentDidMount() {
   }
   render() {
+    const { scale } = this.state;
     return <div className="page-axis-board">
+      <AxisBoard width={800} height={800} scale={scale} />
     </div>
   }
 };
